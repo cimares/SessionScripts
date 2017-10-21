d94c2a145f6b4ca00bac53496289aee963d9fff3
cimares <cimares@users.noreply.github.com>
2017-10-21 14:22:38 +0100

    Upload of Audit Session scripts

---
+++ MakingAuditingGreatAgain/SetupSubscription.ps1
@@ -0,0 +1,153 @@
+﻿. .\SupportFunctions.ps1
+$globalConfig = Get-Content makingAuditGreatAgain-config.json -Raw | ConvertFrom-Json
+
+
+###DEMO
+##Here we get the oAuth access token that we use to authenticate our REST requests.
+##Code for this is in SupportFunctions.ps1
+$oAuth = get-oAuthAccessToken -resource $globalConfig.ResourceAPI -clientID $globalConfig.InvestigationAppID -clientSecret $globalConfig.InvestigationAppSecret -loginURL $globalConfig.LoginURL -tenantDomain $globalConfig.InvestigationTenantDomain
+$header  = @{'Authorization'="$($oAuth.token_type) $($oAuth.access_token)"}
+
+
+Get-CurrentAuditSubscription -tenantGUID $globalConfig.InvestigationTenantGUID -header $header
+
+
+
+##Should we need to recreate a subscription we use:
+#Invoke-RestMethod -Method Post -Headers $headerParams -Uri "https://manage.office.com/api/v1.0/$tenantGUID/activity/feed/subscriptions/start?contentType=Audit.SharePoint"
+
+
+##Request the Manifest for a given date/time range and ContentType
+$extractDate = (get-date).AddDays(-$globalConfig.ProcessLogOffsetDays)
+$processDate = $extractDate.toString("yyyy-MM-dd")
+
+##Forcing the date here just for SPS Belgium
+$processDate = "2017-10-17"
+
+
+#REST request is here. Content type and process window.
+$auditSuffix = "?contentType=Audit.SharePoint&startTime=" + $processDate + "T00:00:00Z&endTime=" + $processDate + "T23:59:59Z"
+
+$blobManifestConsolidated = @()
+$blobList = @()
+
+
+##Now fetch all of the available Blob Manifests for the given time period
+##This may require multiple connections
+do
+{
+    $BlobManifest = fetch-BlobManifest -header $header -tenantGUID $globalConfig.InvestigationTenantGUID -auditSuffix $auditSuffix -nextPageURI $BlobManifest.Headers.NextPageUri
+    $blobManifestConsolidated += $blobManifest
+
+}
+while ($BlobManifest.Headers.NextPageUri)
+
+foreach($blobManifestRetrieved in $blobManifestConsolidated)
+{
+    ##now process each manifest for this date
+    $blobList += $blobManifestRetrieved.Content | ConvertFrom-Json
+}
+
+
+##Now process the Blob list and get the actual data.
+
+$blobTotal = $blobList.count
+$blobCount = 0
+$auditRecords = @()
+write-host "Blob package contains" $blobTotal "packages for processing"
+
+##Setup our Azure SQL Connection
+$connectionString = "Server=tcp:" + $globalConfig.SQLServerURI +
+ ",1433;Database=" + $globalConfig.SQLDatabaseName + ";Uid=" + $globalConfig.SqlAccessAccount + ";Pwd=" + $globalConfig.SqlAccessAccountPwd +
+  ";Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+$conn = new-object System.Data.SqlClient.SqlConnection
+$conn.ConnectionString = $connectionString
+$command = new-object System.Data.SqlClient.SqlCommand
+$command.Connection = $conn
+$recordCount = 0
+
+if ($blobTotal -gt 0)
+{
+    foreach ($blobDataSource in $blobList)
+    {
+        $blobCount++
+        $statusmsg = "Processing blobs"
+        $percentComplete = ($blobCount/$blobTotal)*100
+        Write-Progress -Id 1 -Activity "Processing blob $blobCount of $blobTotal" -status $statusmsg -PercentComplete $percentComplete
+
+        ##Depending on how long this process takes, we could have our app token expire.
+        ##if this happens we need to refresh it.
+        $tokenDate = Convert-FromUnixdate $oAuth.expires_on
+        if ($tokenDate -gt (get-date).AddMinutes(5))
+        {
+            #Token still valid. Continue
+        }
+        else
+        {
+            # Token has less than 5 minutes, so refresh it.
+            # Retrieve Oauth 2 access token
+            write-host "Refreshing oAuth token" -ForegroundColor Yellow
+            $oAuth = get-oAuthAccessToken -resource $globalConfig.ResourceAPI -clientID $globalConfig.InvestigationAppID -clientSecret $globalConfig.InvestigationAppSecret -loginURL $globalConfig.LoginURL -tenantDomain $globalConfig.InvestigationTenantDomain
+            #Create the header params for this token.
+            $header  = @{'Authorization'="$($oAuth.token_type) $($oAuth.access_token)"}
+        }
+
+        $blobData = fetch-BlobContent -header $header -blobURI $blobDataSource.contentUri
+
+        $blobObjects = $blobData.Content | ConvertFrom-Json
+
+
+        ##Now process the Blobobjects and see if we want them or not
+        #Or just copy the lot..
+
+        foreach ($auditEntry in $blobObjects)
+        {
+
+
+            #Apply business logic here!
+            switch -Wildcard ($auditEntry.ObjectId.ToLower())
+            {
+                ##If we match the URL with wildcard, save the entry
+                ##This could push it directly into SQL
+                "https://tenantname.sharepoint.com/sites/audit-test*" {
+
+
+
+                $ObjcreationTime = get-date $auditEntry.CreationTime
+                $objId = $auditEntry.Id
+                $objOperation = $auditEntry.Operation
+                $objWorkload = $auditEntry.Workload
+                $objClientIp = $auditEntry.ClientIP
+                $objEventSource = $auditEntry.EventSource
+                $objObjectId = $auditEntry.ObjectId
+                $objUserId = $auditEntry.UserId
+                $objSite = $auditEntry.Site
+                $objFileExtension = $auditEntry.SourceFileExtension
+
+                ##Now insert the values
+                $command.commandtext = "INSERT INTO AuditRecords (CreationTime,Id,Operation,Workload,ClientIP,EventSource,ObjectId,UserId,Site,SourceFileExtension)" +
+                 " VALUES ('$ObjcreationTime','$objId','$objOperation','$objWorkload','$objClientIp','$objEventSource','$objObjectId','$objUserId','$objSite','$objFileExtension')"
+
+                $command.CommandType = [System.Data.CommandType]::Text
+
+                $conn.Open()
+                $command.ExecuteNonQuery()
+                $conn.close()
+                $recordCount++
+
+                }
+
+
+                default {
+                    ##Default behaviour hit
+                    ##Do nothing as it's not a focus site.
+                    ##We could write this out to a seperate file if we wanted.
+                
+                }
+            }
+
+        }
+    }
+}
+
+write-host "$recordCount entries out of $blobTotal written to SQL"
---
+++ MakingAuditingGreatAgain/SupportFunctions.ps1
@@ -0,0 +1,176 @@
+﻿##Support functions for Making Auditing Great Again!
+
+##Shrink the prompt down
+function Prompt
+{
+    return ">";
+}
+
+##Function to convert the oAuth timestamps into real datetimes.
+Function Convert-FromUnixdate ($UnixDate) {
+
+  [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').`
+
+  AddSeconds($UnixDate))
+
+}
+
+function get-oAuthAccessToken()
+{
+    Param(
+        [Parameter(Mandatory=$true)] [String] $resource,
+        [Parameter(Mandatory=$true)] [String] $clientID,
+        [Parameter(Mandatory=$true)] [String] $clientSecret,
+        [Parameter(Mandatory=$true)] [String] $loginURL,
+        [Parameter(Mandatory=$true)] [String] $tenantDomain
+    )   
+
+    # Retrieve Oauth 2 access token
+    $body = @{grant_type="client_credentials";resource=$resource;client_id=$clientID;client_secret=$clientSecret}
+    $oauthToken = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
+
+    return $oauthToken
+}
+
+
+function Get-CurrentAuditSubscription()
+{
+    Param(
+        [Parameter(Mandatory=$true)] [string] $tenantGUID,
+        [Parameter(Mandatory=$true)] [Hashtable] $header
+    )  
+    $result = $false
+    #Validate named Subscription is active.
+    #If it isn't something has gone wrong with the O365 configuration or it's been removed.
+    $subs = Invoke-WebRequest -Headers $header -Uri "https://manage.office.com/api/v1.0/$tenantGUID/activity/feed/subscriptions/list" | Select Content
+
+    if ($subs.content.Length -le 2)
+    {
+        Write-Host "No audit subscriptions returned"
+        return $false
+    }
+    else
+    {
+        $subsObject = $subs.content | ConvertFrom-Json
+        return $subsObject
+    }
+
+    return $null
+    
+}
+
+
+
+function fetch-BlobManifest
+{
+    Param(
+        [Parameter(Mandatory=$true)] [Hashtable] $header,
+        [Parameter(Mandatory=$true)] [string] $tenantGUID,
+        [Parameter(Mandatory=$true)] [string] $auditSuffix,
+        [Parameter(Mandatory=$false)] [string] $nextPageURI
+    )      
+
+    $requestURI = "https://manage.office.com/api/v1.0/$tenantGUID/activity/feed/subscriptions/content$auditSuffix"
+
+    if ($nextPageURI)
+    {
+        $requestURI = $nextPageURI
+    }
+
+    $retryCount = 0
+    $blobManifest = $null
+    $callSuccess = $false
+    do
+    {
+        $retryCount++
+        if ($retryCount -gt 50)
+        {
+            throw "Maximum retry count hit"
+        }
+        try
+        {
+            $blobManifest = Invoke-WebRequest -Headers $header -Uri $requestURI
+            $callSuccess = $true
+        }
+        catch 
+        {
+            $errorCaught = $error[0].ErrorDetails
+            if ($errorCaught.Message)
+            {
+                $errorJSON = $errorCaught.Message | convertfrom-json
+                if ($errorJSON.error.code -eq "AF429")
+                {
+                    #Need to back off for half a second.
+                    write-host "Back off algorithm hit" -ForegroundColor Yellow
+                    Write-Output "Back off algorithm hit"
+                    start-sleep -m 500
+                                
+                }
+                else
+                {
+                    throw
+                }
+            }
+            else
+            {
+                throw
+            }
+
+        }
+    }
+    until ($callSuccess -eq $true)
+
+    return $blobManifest
+}
+
+function fetch-BlobContent
+{
+    Param(
+        [Parameter(Mandatory=$true)] [Hashtable] $header,
+        [Parameter(Mandatory=$false)] [string] $blobURI
+    )      
+
+    $retryCount = 0
+    $callSuccess = $false
+    do
+    {
+        $retryCount++
+        if ($retryCount -gt 50)
+        {
+            throw "Maximum retry count hit"
+        }
+        try
+        {
+            $blobData = Invoke-WebRequest -Headers $header -Uri $blobURI
+            $callSuccess = $true
+        }
+        catch 
+        {
+            $errorCaught = $error[0].ErrorDetails
+            if ($errorCaught.Message)
+            {
+                $errorJSON = $errorCaught.Message | convertfrom-json
+                if ($errorJSON.error.code -eq "AF429")
+                {
+                    #Need to back off for half a second.
+                    write-host "Back off algorithm hit" -ForegroundColor Yellow
+                    Write-Output "Back off algorithm hit"
+                    start-sleep -m 500
+                                
+                }
+                else
+                {
+                    throw
+                }
+            }
+            else
+            {
+                throw
+            }
+
+        }
+    }
+    until ($callSuccess -eq $true)
+
+    return $blobData
+}
